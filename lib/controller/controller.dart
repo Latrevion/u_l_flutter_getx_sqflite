4cf1b22d65298401c9fe1479355862ed9c0d5e33
luzhihong <xiaopikaqiu@outlook.com>
2023-11-17 20:23:49 +0800

    insert and get data from database

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -4,12 +4,13 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class SQLController extends GetxController {
-
   @override
-  void onInit(){
+  void onInit() {
     createDatabase();
     super.onInit();
   }
+
+  late Database database;
 
   void createDatabase() async {
     //get a location using getDatabasePath
@@ -19,7 +20,7 @@ class SQLController extends GetxController {
     openAppDatabase(path: path);
   }
 
-  void deleteTheDatabase() async{
+  void deleteTheDatabase() async {
     var databasePath = await getDatabasesPath();
     String path = join(databasePath, 'todo.db');
     await deleteDatabase(path);
@@ -28,29 +29,42 @@ class SQLController extends GetxController {
 
   void openAppDatabase({required String path}) async {
     //open the database
-    Database database = await openDatabase(
+    await openDatabase(
       path,
       version: 1,
       onCreate: (Database db, int version) async {
         // When creating the db, create the table
         await db.execute(
-            'CREATE TABLE todo (id INTEGER PRIMARY KEY, title TEXT,description TEXT,time favorite INTEGER, completed REAL)');
+            'CREATE TABLE todo (id INTEGER PRIMARY KEY, title TEXT,description TEXT,time Text,favorite INTEGER, completed INTEGER)');
         debugPrint('database is Created');
       },
-      onOpen: (Database database){
+      onOpen: (Database db) {
+        database = db;
+        getAllData();
         debugPrint('database is opened');
       },
-
     );
   }
 
-  void getAllData(){}
+  void getAllData() async {
+    var allData = await database.query('todo');
+    print(allData);
+    update();
+  }
 
-  void insertData(){}
+  void insertData() async {
+    var insert = await database.insert('todo', {
+      'title': 'go',
+      'description': 'go to school',
+      'time': '10',
+      "favorite": 0,
+      "completed": 0
+    });
+    debugPrint('data insert =>$insert');
+    getAllData();
+  }
 
-  void updateData(){}
+  void updateData() {}
 
-  void deleteData(){}
-
-
+  void deleteData() {}
 }
--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:u_l_flutter_getx_sqflite/controller/controller.dart';
 import 'package:get/get.dart';
 
-
 class HomeScreen extends StatelessWidget {
-   HomeScreen({super.key});
+  HomeScreen({super.key});
 
-   final controller = Get.put(SQLController());
+  final controller = Get.put(SQLController());
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title:const Text('todo app'),
-      centerTitle: true,
-      actions: [
-        IconButton(onPressed: (){
-          controller.deleteTheDatabase();
-        }, icon: const Icon(Icons.remove))
-      ],
-    ),
-  );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('todo app'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                controller.deleteTheDatabase();
+              },
+              icon: const Icon(Icons.remove)),
+          IconButton(
+              onPressed: () {
+                controller.insertData();
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+    );
   }
 }
