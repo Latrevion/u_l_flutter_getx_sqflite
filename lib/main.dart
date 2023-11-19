e202e31f99ce926ac0f54185b7dfcf0d8c43e285
luke <xiaopikaqiu@outlook.com>
2023-11-19 22:08:35 +0800

    make a simple ui 1

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      themeMode: ThemeMode.dark,
       home: HomeScreen(),
     );
   }
 }
-
-
